52b70ec81a5b19ed02dfba978b3096b6dd04af69
NathteeP <whatisitz.plus@gmail.com>
2024-05-07 11:22:16 +0700

    setup router to pages

---
+++ src/routes/Router.jsx
@@ -0,0 +1,37 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import HomePage from "../pages/HomePage"
+import LoginPage from "../pages/LoginPage"
+import ProfilePage from "../pages/ProfilePage"
+import NotFound from "../pages/NotFound"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <HomePage />
+        )
+    },
+    {
+        path: '/login',
+        element: (
+            <LoginPage />
+        )
+    },
+    {
+        path: '/profile',
+        element: (
+            <ProfilePage />
+        )
+    },
+    {
+        path: '*',
+        element: (
+            <NotFound />
+        )
+    }
+])
+function Router () {
+    return <RouterProvider router={router} />
+}
+
+export default Router
